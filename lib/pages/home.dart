4dc41d332db65e4fd041f1a1917d4cd97f3d859a
schmeekygeek <ss6087982@gmail.com>
2023-12-13 22:47:58 +0530

    move pages to pages/

---
+++ lib/pages/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Hi",
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+      ),
+    );
+  }
+}
