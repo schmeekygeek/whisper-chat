8a99a9c106c122176b128d3e3c9f61e1d0e9778e
schmeekygeek <ss6087982@gmail.com>
2024-03-15 20:11:06 +0530

    make chat chip

--- lib/shared/chat_chip.dart
+++ lib/shared/chat_chip.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whisper_chat/providers/message_provider.dart';
 
 class ChatChip extends StatelessWidget {
   final String text;
@@ -7,14 +9,28 @@ class ChatChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 90,
-      child: Column(
-        children: [
-          Text(from),
-          Text(text),
-          const Divider(),
-        ],
+
+    bool isSelf = context.read<MessageProvider>().getSelfUsername == from;
+
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Align(
+        alignment: isSelf ? Alignment.centerRight : Alignment.centerLeft,
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey,
+            borderRadius: BorderRadius.only(
+              topLeft: isSelf ? const Radius.circular(10) : const Radius.circular(0),
+              topRight: !isSelf ? const Radius.circular(10) : const Radius.circular(0),
+              bottomLeft: const Radius.circular(10),
+              bottomRight: const Radius.circular(10),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Text(text),
+          ),
+        ),
       ),
     );
   }
